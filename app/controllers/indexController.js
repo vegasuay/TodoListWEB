97c727abb2b23ed68a4cbcefdcb3d7bae7f280d7
Roberto Vega <roberto.vegas@tecnocom.es>
2017-11-06 12:56:48 +0100

    app entera

---
+++ app/controllers/indexController.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+app.controller('indexController', ['$scope', '$location', 'authService',
+    function ($scope, $location, authService) {
+
+        $scope.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
+        };
+
+        $scope.logOut = function () {
+            authService.logOut();
+            $location.path('/home');
+        }
+
+        $scope.authentication = authService.authentication;
+    }
+]);
